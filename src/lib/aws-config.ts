5fc9b5a19db94a2cf53d8b8bc4544b9f2973ddc0
Rahsaan27 <rahsaan@hiddengeniusproject.org>
2025-12-10 12:42:35 -0800

    Attempt 5

--- src/lib/aws-config.ts
+++ src/lib/aws-config.ts
@@ -1,9 +1,9 @@
 // OIDC configuration for AWS Cognito
 export const oidcConfig = {
-  authority: 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_6LxsAjtRX',
-  client_id: '4botmnmnknikbipc801vbsgvta',
-  redirect_uri: 'http://localhost:3001/callback',
-  post_logout_redirect_uri: 'http://localhost:3001',
+  authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY || 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_6LxsAjtRX',
+  client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || '4botmnmnknikbipc801vbsgvta',
+  redirect_uri: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI || 'http://localhost:3001/callback',
+  post_logout_redirect_uri: process.env.NEXT_PUBLIC_COGNITO_LOGOUT_URI || 'http://localhost:3001',
   response_type: 'code',
   scope: 'phone openid email'
 };
